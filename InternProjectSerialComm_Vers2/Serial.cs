be7388c1a8269bd35b17232f358300c000cd4e45
cowan@DESKTOP-NDM70SL.Chris <cowan@DESKTOP-NDM70SL.cowancm2003@gmail.com>
2024-06-21 18:02:28 -0500

    First commit...

---
+++ InternProjectSerialComm_Vers2/Serial.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.IO.Ports;
+
+namespace SerialToUSBCommunication
+{
+    public class Serial
+    {
+        private readonly string? _COMPort;
+
+        private SerialPort _port;
+
+        public enum BAUD_RATE
+        {
+            FortyEightHundred = 4800,
+            NinetySixHundred = 9600,
+            NineteenThousandTwoHundred = 19200,
+            ThirtyEightThousandFourHundred = 38400,
+            SeventySixThousandEightHundred = 76800,
+            OneHundredFifteenThousandTwoHundred = 115200
+        }
+
+        public Serial(BAUD_RATE baudEnum = BAUD_RATE.OneHundredFifteenThousandTwoHundred, Parity Parity = Parity.None, string DeviceName = "Silicon Labs CP210x USB to UART Bridge")
+        {
+            _COMPort = null;
+
+            //Grab port that matches the serial driver we're using
+            foreach (string portNameOnDevice in SerialPort.GetPortNames())
+            {
+                if (IsDesiredDevice(portNameOnDevice, DeviceName))
+                {
+                    _COMPort = portNameOnDevice;
+                    break;
+                }
+            }
+
+            //throw error if never found...
+            if (_COMPort == null)
+                throw new Exception("Device not found! Check your USB connection, make sure there are no programs actively using this port, and ensure \"Silicon Labs CP210x USB to UART Bridge\" driver is installed correctly. The driver can be installed at:\nhttps://www.silabs.com/developers/usb-to-uart-bridge-vcp-drivers?tab=downloads\n\n Restart the program.");
+
+            _port = new()
+            {
+                PortName = _COMPort,
+                BaudRate = (int) baudEnum,
+                Parity = Parity.None,
+                DataBits = 8
+            };
+
+            _port.Open();
+        }
+
+
+        private static bool IsDesiredDevice(string portName, string desiredDeviceDescription)
+        {
+            string query = "SELECT * FROM Win32_PnPEntity WHERE Name LIKE '%" + portName + "%'";
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(query))
+            {
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    string description = obj["Description"]?.ToString();
+                    if (description != null && description.Contains(desiredDeviceDescription))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public void open()
+        {
+            _port.Open();
+        }
+
+        public void Close()
+        {
+            _port.Close();
+        }
+
+        public void ChangeBAUD(BAUD_RATE BaudEnum)
+        {
+            _port.BaudRate = (int)BaudEnum;
+        }
+
+        public void ChangeDeviceOrCOM(string DeviceName)
+        {
+            _port.PortName = DeviceName;
+        }
+
+        public string ReadFromPort()
+        {
+            return _port.ReadLine();
+        }
+
+        public void WriteToPort(string UART)
+        {
+            //write with carridge return and new line
+            _port.Write(UART + "\r\n");
+        }
+
+        public async Task<string> ReadFromPortAsync()
+        {
+            return await Task.Run(() => _port.ReadLine());
+        }
+
+        public async Task WriteToPortAsync(string UART)
+        {
+            await Task.Run(() => _port.Write(UART + "\r\n"));
+        }
+    }
+}
---
+++ InternProjectSerialComm_Vers2/main.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialToUSBCommunication
+{
+    //note: I coded this for a command line, maybe add an interface?
+
+    //TODO:
+    //1. Make an interface IControl that outlines the contracts that will let us control the vehicle from
+    //   * Interface should have contracts for move forward, turn right, turn left etc so for example Keyboard controller class and xbox controller class implements Icontrol
+    //
+    //2. Add logging for GPS and movement data w/ time stamps (per Alvin) -> make class that implements ILogger, poops out txt files with this information (add hashing for security purposes? (also per Alvin))
+    //3. Maybe make some type of graphical interface instead of jank cmd?
+
+
+    public class main
+    {
+        public static async Task Main()
+        {
+            Console.WriteLine("Input the BAUD rate of your device by inputing the corresponding number:\n");
+            bool incorrectBAUDCheck = true;
+            Serial.BAUD_RATE baudRate = Serial.BAUD_RATE.OneHundredFifteenThousandTwoHundred;
+
+            while (incorrectBAUDCheck)
+            {
+                Console.WriteLine("[1]\t\t  4800 BPS");
+                Console.WriteLine("[2]\t\t  9600 BPS");
+                Console.WriteLine("[3]\t\t 19200 BPS");
+                Console.WriteLine("[4]\t\t 38400 BPS");
+                Console.WriteLine("[5]\t\t 76800 BPS");
+                Console.WriteLine("[6]\t\t115200 BPS\n\n");
+
+                var input = Console.ReadLine();
+
+
+                if ((string.IsNullOrWhiteSpace(input)))
+                {
+                    Console.WriteLine("Error, please try again");
+                    continue;
+                }
+
+                int inputShouldBeNum;
+
+                try
+                {
+                    inputShouldBeNum = int.Parse(input);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error, please try again");
+                    continue;
+                }
+
+                switch (inputShouldBeNum)
+                {
+                    case 1:
+                        baudRate = Serial.BAUD_RATE.FortyEightHundred;
+                        break;
+                    case 2:
+                        baudRate = Serial.BAUD_RATE.NinetySixHundred;
+                        break;
+                    case 3:
+                        baudRate = Serial.BAUD_RATE.NineteenThousandTwoHundred;
+                        break;
+                    case 4:
+                        baudRate = Serial.BAUD_RATE.ThirtyEightThousandFourHundred;
+                        break;
+                    case 5:
+                        baudRate = Serial.BAUD_RATE.SeventySixThousandEightHundred;
+                        break;
+                    case 6:
+                        baudRate = Serial.BAUD_RATE.OneHundredFifteenThousandTwoHundred;
+                        break;
+                    default:
+                        Console.WriteLine("Error, please try again");
+                        continue;
+                }
+
+                incorrectBAUDCheck = false;
+            }
+
+            Console.WriteLine("\nAttempting to connect to port...");
+
+            var comPort = new Serial(baudRate);
+
+            Task readTask = Task.Run(() => ReadFromPort(comPort));
+            Task writeTask = Task.Run(() => WriteCommandsToPort(comPort));
+
+            await Task.WhenAll(readTask, writeTask);
+
+            Console.ReadLine();
+        }
+
+        public static async Task ReadFromPort(Serial port)
+        {
+            while (true)
+            {
+                string data = port.ReadFromPort();
+
+                if (data != null)
+                {
+                    Console.WriteLine(data);
+                }
+
+                await Task.Delay(10);
+            }
+        }
+
+        public static async Task WriteCommandsToPort(Serial port)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("Empty input, please try again...");
+                    continue;
+                }
+
+                CheckIfUserWantsToStop(input, port);
+
+                port.WriteToPort(input);
+                await Task.Delay(10);
+            }
+        }
+
+        public static void CheckIfUserWantsToStop(string input, Serial port)
+        {
+            if ((input != "stop")||(input != "Stop"))
+            {
+                Console.WriteLine("Ending Operation... It was a pleasure :)");
+                
+                Environment.Exit(0);
+                port.Close();
+            }
+        }
+
+
+    }
+}
