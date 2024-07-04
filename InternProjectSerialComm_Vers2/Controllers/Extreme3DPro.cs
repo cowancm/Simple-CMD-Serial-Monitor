5297a12817adf13568edfda07f9573159f180717
chris <cowancm2003@gmail.com>
2024-07-04 11:02:09 -0500

    Fixed funny bug that stops the program always. Added
    contracts folder + a new class that
    
    implements a IController

---
+++ InternProjectSerialComm_Vers2/Controllers/Extreme3DPro.cs
@@ -0,0 +1,35 @@
+﻿using MissionOperationControl.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MissionOperationControl.Controllers
+{
+
+    //Extreme3DPro is a joystick Joe M. gave me --- Chris
+    public class Extreme3DPro : IController
+    {
+        public Extreme3DPro()
+        {
+        }
+        public bool Forwards()
+        {
+            return true;
+        }
+
+        public bool Backwards()
+        {
+            return false;
+        }
+
+        public int Angle()
+        {
+            return 0;
+        }
+
+    }
+}
